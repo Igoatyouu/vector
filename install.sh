0af298e66571d697217921378652af47862a5c9c
Axel Tacheau <axel.tacheau@epitech.eu>
2023-04-20 17:53:02 +0200

    [ADD] Mans installation for vectors

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+##
+## EPITECH PROJECT, 2023
+## sh
+## File description:
+## install man vectors
+##
+
+sudo mkdir /usr/local/man/man3/
+sudo cp mans/* /usr/local/man/man3/
+echo "mans installed successfully !"
